6dba01b57e2780087bba8984e933d6abc2eb8d0a
Christian De Frene <christian.de.frene@bos.no>
2020-02-19 14:24:55 +0100

    Cleanup existing code

--- toggl/toggl-start-timer.js
+++ toggl/toggl-start-timer.js
@@ -1,12 +1,37 @@
-const url = 'https://www.toggl.com/api/v8/time_entries/start';
-const api_token = process.env.TOGGL_API_TOKEN
-const body = {
-	"time_entry": {
-		"description": "Auto-added timer entry",
-		"wid": process.env.TOGGL_WORKSPACE_ID,
-		"pid": process.env.TOGGL_PROJECT_ID,
-		"created_with": "Github actions"
-	}
+const fetch = require('node-fetch');
+
+const authHeader = require('./auth-header');
+
+/*
+ Start a time entry
+
+ API documentation:
+ https://github.com/toggl/toggl_api_docs/blob/master/chapters/time_entries.md#start-a-time-entry
+ */
+async function startTimer() {
+    const url = 'https://www.toggl.com/api/v8/time_entries/start';
+    const data = {
+        "time_entry": {
+            "description": "Auto-added timer entry",
+            "wid": process.env.TOGGL_WORKSPACE_ID,
+            "pid": process.env.TOGGL_PROJECT_ID,
+            "created_with": "Github actions"
+        }
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: authHeader,
+        body: JSON.stringify(data)
+    });
+
+    return await response.json();
 }
 
-const response = fetch(url, {method: 'POST', })
+// startTimer()
+//     .then(json => {
+//         console.log(json);
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     });
